95b4789cc41cd173ca75522430aa89f736edec9e
Fredd Castillo <freddy0fh@localhost.localdomain>
2019-05-31 00:42:26 -0500

    Registro con FormBuilder

--- src/app/components/save-user-data/save-user-data.component.ts
+++ src/app/components/save-user-data/save-user-data.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { AuthService } from 'src/app/shared/services/auth.service';
 import { BaseComponent } from '../base.component';
 import { MediaMatcher } from '@angular/cdk/layout';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-save-user-data',
@@ -9,6 +10,17 @@ import { MediaMatcher } from '@angular/cdk/layout';
   styleUrls: ['./save-user-data.component.less']
 })
 export class SaveUserDataComponent extends BaseComponent implements OnInit {
+
+  userForm = this.fb.group({
+    type: [null, Validators.required],
+    company: null,
+    names: [null, Validators.required],
+    lastName: [null, Validators.required],
+    address: [null, Validators.required],
+    identificacion: [null, Validators.required],
+    birthDate: null,
+    sexo: null
+  });
 
   listSexo: any[] = [
     {value: 'Masculino', viewValue: 'Masculino'},
@@ -22,11 +34,23 @@ export class SaveUserDataComponent extends BaseComponent implements OnInit {
   constructor(
     public authService: AuthService,
     private med: MediaMatcher,
-    private cdr: ChangeDetectorRef) { 
+    private cdr: ChangeDetectorRef,
+    private fb: FormBuilder) { 
     super(med, cdr);
   }
 
   ngOnInit() {
+  }
+  onSubmit() {
+    this.authService.finishSaveData(
+      this.userForm.controls.identificacion.value,
+      this.userForm.controls.names.value,
+      this.userForm.controls.lastName.value,
+      this.userForm.controls.birthDate.value,
+      this.userForm.controls.sexo.value,
+      this.userForm.controls.type.value,
+    );
+
   }
 
 }
