aae6c6baa5e74ce9d1cc7ab6d51951832075985f
Fredd Castillo <freddy0fh@localhost.localdomain>
2019-06-02 07:36:24 -0500

    Eliminado dist

--- dist/giflo/firebase-messaging-sw.js
+++
@@ -1,8 +0,0 @@
-importScripts('https://www.gstatic.com/firebasejs/5.7.3/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/5.7.3/firebase-messaging.js');
-
-firebase.initializeApp({
-    messagingSenderId: '487738962539'
-});
-
-const messaging = firebase.messaging();
