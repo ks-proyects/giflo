45a6a443d9b7a192c8268e379f45d4b9cc66336a
Freddy Castillo <freddy.castillo@bayteq.com>
2020-04-10 21:40:04 -0500

    FCA Nuevo Modulos

---
+++ src/app/app.routing.ts
@@ -0,0 +1,88 @@
+// DEPENDENCIES
+import { Routes } from '@angular/router';
+// SECURITY
+import { AuthGuard } from './security/auth.guard';
+import { LandingComponent } from './layout/landing/landing.component';
+import { FullComponent } from './layout/full/full.component';
+import { AppBlankComponent } from './layout/blank/blank.component';
+/**
+ * WEB APP ROUTES
+ */
+export const AppRoutes: Routes = [
+  {
+    path: '',
+    component: LandingComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: './pages/public/public.module#PublicModule'
+      },
+    ]
+  },
+  {
+    path: '',
+    component: FullComponent,
+    children: [
+      {
+        path: 'admin',
+        loadChildren: './pages/private/admin/admin.module#AdminModule'
+      },
+    ],
+    canActivate: [AuthGuard]
+  },
+  {
+    path: '',
+    component: FullComponent,
+    children: [
+      {
+        path: 'catalog',
+        loadChildren: './pages/private/catalog/catalog.module#CatalogModule'
+      },
+    ],
+    canActivate: [AuthGuard]
+  },
+  {
+    path: '',
+    component: FullComponent,
+    children: [
+      {
+        path: 'management',
+        loadChildren: './pages/private/management/management.module#ManagementModule'
+      },
+    ],
+    canActivate: [AuthGuard]
+  },
+  {
+    path: '',
+    component: FullComponent,
+    children: [
+      {
+        path: 'security',
+        loadChildren: './pages/private/security/security.module#SecurityModule'
+      },
+    ],
+    canActivate: [AuthGuard]
+  },
+  {
+    path: '',
+    component: FullComponent,
+    children: [
+      {
+        path: 'home',
+        loadChildren: './pages/private/home/home.module#HomeModule'
+      },
+    ],
+    canActivate: [AuthGuard]
+  },
+  {
+    path: '',
+    component: AppBlankComponent,
+    children: [
+      {
+        path: 'authentication',
+        loadChildren:
+          './pages/authentication/authentication.module#AuthenticationModule'
+      }
+    ]
+  },
+];
---
+++ src/app/core.service.module.ts
@@ -0,0 +1,54 @@
+import { NgModule } from '@angular/core';
+
+/* START MY SERVICES IMPORTS */
+// Do not edit this comment content, it will be overwritten in next Skaffolder generation
+import { BloqueService } from './services/bloque.service';
+import { CamaService } from './services/cama.service';
+import { ContactoService } from './services/contacto.service';
+import { DireccionService } from './services/direccion.service';
+import { EmpleadoService } from './services/empleado.service';
+import { EmpresaService } from './services/empresa.service';
+import { EstadoService } from './services/estado.service';
+import { EstadoCivilService } from './services/estado-civil.service';
+import { MenuItemService } from './services/menu-item.service';
+import { NaveService } from './services/nave.service';
+import { PaginaService } from './services/pagina.service';
+import { RolService } from './services/rol.service';
+import { UserService } from './services/user.service';
+import { VariedadService } from './services/variedad.service';
+
+/* END MY SERVICES IMPORTS */
+
+import { AuthGuard } from './security/auth.guard';
+import { AuthenticationService } from './security/authentication.service';
+import { PersonaService } from './services/persona.service';
+
+@NgModule({
+  imports: [],
+  providers: [
+    /* START PROVIDERS */
+// Do not edit this comment content, it will be overwritten in next Skaffolder generation
+    BloqueService,
+    CamaService,
+    ContactoService,
+    DireccionService,
+    EmpleadoService,
+    EmpresaService,
+    EstadoService,
+    EstadoCivilService,
+    MenuItemService,
+    NaveService,
+    PaginaService,
+    RolService,
+    UserService,
+    VariedadService,
+    PersonaService,
+ /* END PROVIDERS */
+    // SECURITY
+    AuthGuard,
+    AuthenticationService
+  ],
+  exports: []
+})
+export class CoreServiceModule {
+}
