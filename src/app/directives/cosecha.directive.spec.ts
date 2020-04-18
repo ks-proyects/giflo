7d3744ab4f0a7b5d8d98a91eaec36e3714dc10c9
Freddy Castillo <freddy.castillo@bayteq.com>
2020-04-18 11:08:04 -0500

    Add Directs and Model

---
+++ src/app/directives/cosecha.directive.spec.ts
@@ -0,0 +1,8 @@
+import { CosechaDirective } from './cosecha.directive';
+
+describe('CosechaDirective', () => {
+  it('should create an instance', () => {
+    const directive = new CosechaDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/domain/giflo_db/dia-trabajo.ts
@@ -0,0 +1,9 @@
+import { DiaTrabajoBase } from './base/dia-trabajo.base';
+
+/**
+ * YOU CAN OVERRIDE HERE DiaTrabajoBase.ts
+ */
+export interface DiaTrabajo extends DiaTrabajoBase {
+
+    // Insert here your custom attributes and function
+}
